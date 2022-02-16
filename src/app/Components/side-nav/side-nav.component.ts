ae9d0cc2f407ae78f179477587f9bec89aa567b7
Yeiko <yeimaraya91@gmail.com>
2022-02-16 10:09:24 -0600

    Ejemplo tabla

--- src/app/Components/side-nav/side-nav.component.ts
+++ src/app/Components/side-nav/side-nav.component.ts
@@ -16,6 +16,7 @@ export class SideNavComponent implements OnInit {
   mostrarHora: boolean = true;
   visibleSidebar: any;
   participantes: string[] = [];
+  //form: FormGroup;
 
   temaColores: any[] = [
     {nombre: 'Rojo', code: '#EF9A9A'},
@@ -28,9 +29,34 @@ export class SideNavComponent implements OnInit {
       {nombre: 'Naranja', code: '#FFCC80'},
   ];
 
+  activities: any[] = [
+    {
+      title: 'Defensa proyecto',
+      allDay: false,
+      startDate: '2022-02-20',
+      startHour: '10:30:00',
+      endDate: '2022-02-20',
+      endHour: '11:30:00',
+      description: 'Defensa del proyecto de programación avanzada, cod: 03125',
+      participants: 'mariacastro@uned.ac.cr, kimberly.solano@uned.ac.cr',
+      backgroundColor: 'Azul claro'
+    }
+  ]
+
   constructor(private fb: FormBuilder) { 
     
-
+    /*this.form = this.fb.group({
+      title: ['', Validators.required],
+      days: [''],
+      allDay: [''],
+      startDate: ['', Validators.required],
+      startHour: ['', Validators.required],
+      endDate: ['', Validators.required],
+      endHour: ['', Validators.required],
+      description: [''],
+      participants: [''],
+      backgroundColor: ['']
+    })*/
   
   }
 
@@ -41,6 +67,19 @@ export class SideNavComponent implements OnInit {
 
 
   saveData(){
+
+    /* const activity: any = {
+       title: this.form.get('title'),
+       allDay: this.form.get('allDay'),
+       startDate: this.form.get('startDate'),
+       startHour: this.form.get('startHour'),
+       endDate: this.form.get('endDate'),
+       endHour: this.form.get('endHour'),
+       description: this.form.get('description'),
+       participants: this.form.get('participants'),
+       backgroundColor: this.form.get('backgroundColor')
+     }*/
+
 
   }
 
